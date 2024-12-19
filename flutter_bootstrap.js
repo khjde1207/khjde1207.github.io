3b6cff9147ae12c72c2bb32f6bbca274dee4ca68
자유해결사 <khjde1207@gmail.com>
2024-12-19 22:05:30 +0900

    8.5.6+856

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2407462533"
+    serviceWorkerVersion: "510586823"
   }
 });
