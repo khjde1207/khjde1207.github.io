d94e16604908481c8417f45154320e64d12de1f6
자유해결사 <khjde1207@gmail.com>
2026-06-06 23:30:32 +0900

    9.3.3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2268227063" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3627045238" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
